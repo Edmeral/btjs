4cc6e030559ea0e5349b63fed3af9d543c2a27f3
Aissam BELHACHMI <edmeral.aissam@gmail.com>
2016-09-05 20:13:26 +0100

    Getting peers from announce response and passing them to a callback

--- torrent.js
+++ torrent.js
@@ -10,7 +10,9 @@ let torrent = bencode.decode(fs.readFileSync('song.torrent'))
 let tracker = url.parse(torrent.announce.toString())
 let peerId = crypto.randomBytes(20) // unique 20 bytes id for this client
 
-// getPeers(tracker)
+getPeers(tracker, peers => {
+  console.log(peers[0])
+})
 
 function getPeers(tracker, cb) {
   let connectReq = makeConnectReq()
@@ -23,19 +25,30 @@ function getPeers(tracker, cb) {
       let transactionIdRes = msg.readInt32BE(4)
 
       // if it's is a connect response
-      if (action == 0 && transactionIdRes == transactionId)  {
+      if (action == 0 && transactionIdRes == transactionId) {
 
         // Getting the connection id
         let connectionId = msg.slice(8, 16)
-        console.log('made connect req, got rest and now making announce req')
-
-        // making announce request
-
+        
+        // Sending announce request
+        let announceReq = makeAnnounceReq(connectionId)
+        transactionId = announceReq.readInt32BE(12)
+        client.send(announceReq, 0, announceReq.length, 
+          tracker.port, tracker.hostname)
+      }
+      
+      // if it's the announce response
+      else if (action == 1 && transactionIdRes == transactionId) {
+        cb(getPeersFromAnnounce(msg))
+      }
+      else {
+        console.error('Error while getting a response from the tracker')
+        client.close()
       }
     })
     
     .on('error', err => {
-      console.log(`Error while making connection with the tracker:\n${err.stack}`)
+      console.error(`Error while making connection with the tracker:\n${err.stack}`)
       client.close();
     })
   
@@ -73,6 +86,8 @@ function makeAnnounceReq(connectionId) {
   crypto.randomBytes(4).copy(announceReq, 88) // key (random 4 bytes)
   announceReq.writeInt32BE(-1, 92) // num_want: numbers of peers wanted (default: -1) 
   announceReq.writeInt16BE(6881, 96) // port (default: 6881-6889)
+
+  return announceReq
 }
 
 function getInfoHash() {
@@ -87,4 +102,17 @@ function getTorrentSize() {
   let buf = Buffer.alloc(8)
   buf.writeInt32BE(torrent.info.length, 4) // assuming length is 32-bit at most
   return buf
+}
+
+function getPeersFromAnnounce(announceRes) {
+  let peersBuf = announceRes.slice(20) 
+  let peers = []
+  for (let i = 0; i < peersBuf.length; i += 6) {
+    let peer = { 
+      ip: peersBuf.slice(i, i + 4).join('.'),
+      port: peersBuf.readUInt16BE(i + 4) 
+    }
+    peers.push(peer)
+  }
+  return peers
 }
